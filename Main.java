ba2a74b2da8f45d4f4a34fc846aa00547025f2ec
Areyes116 <adrian.reyes2006@icloud.com>
2024-10-09 10:39:22 -0700

    Arise

--- Main.java
+++ Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main
 {
-  public static void main(String[] args)
-  {
-    // write solutions to problems here
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		// Part A
+		System.out.println("Problem 1\n");
+		
+		Circle c1 = new Circle(10.1);
+		Circle c2 = new Circle(14.0);
+		Circle c3 = new Circle(20.5);
+
+		System.out.println(c1.toString() + "\n" + c2.toString() + "\n" + c3.toString());
   }
 }
